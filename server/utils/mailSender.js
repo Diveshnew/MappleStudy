02d1da12c4397bf2b88fdacea0e03641d6663455
Divesh Dhek <workwithdivesh@gmail.com>
2025-04-19 15:26:57 +0530

    Chore: Update email transport settings (port and secure) for improved compatibility

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -5,8 +5,8 @@ const mailSender = async (email, title, body) => {
     let transporter = nodemailer.createTransport({
       service: 'Gmail', // Use Gmail (or replace with your service)
       host: process.env.MAIL_HOST,
-      port: 587, // Use 465 for SSL, 587 for TLS
-      secure: false, // True for SSL, false for TLS
+      port: 465, // Use 465 for SSL, 587 for TLS
+      secure: true, // True for SSL, false for TLS
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
