16ce18bc15211856fc95f46cf10eb4be12e1fc8a
Divesh Dhek <workwithdivesh@gmail.com>
2025-04-23 17:53:01 +0530

    feat(home): add "Become an Instructor" button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
-  return <div className="text-red-500">hello</div>;
+  return (
+    <div className="w-screen min-h-screen bg-[var(--color-richblack-900)] flex flex-col font-[var(--font-inter)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      {/* Section 1 */}
+      <div className="relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white justify-between gap-8">
+        {/* Become an Instructor Button */}
+        <Link to={'/signup'}>
+          <div className="group mt-16 p-1 mx-auto gap-2 rounded-full bg-[var(--color-richblack-800)] font-bold text-[var(--color-richblack-200)] transition-all duration-200 hover:scale-95 w-fit drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] hover:drop-shadow-none">
+            <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-[var(--color-richblack-900)]">
+              <p>Become an Instructor</p>
+              <FaArrowRight />
+            </div>
+          </div>
+        </Link>
+      </div>
+      {/* Section 2 */}
+
+      {/* Section 3 */}
+
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default Home;
