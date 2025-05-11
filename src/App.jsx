6d2ac99a41c1f00b2f74e56a55e30b4e851d5915
Divesh Dhek <workwithdivesh@gmail.com>
2025-05-11 12:38:13 +0530

    feat(routes): set up basic routing with Login and Signup routes using OpenRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Navbar from './components/common/Navbar';
+import OpenRoute from './components/core/Auth/OpenRoute';
 
 function App() {
   return (
-    <div className="w-screen min-h-screen bg-[var(--color-richblack-900)] flex flex-col font-[var(--font-inter)]">
+    <div className="flex-1 w-screen bg-[var(--color-richblack-900)] flex flex-col font-[var(--font-inter)]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
       </Routes>
     </div>
   );
