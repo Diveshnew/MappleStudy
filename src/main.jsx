98f94d90de5f24c3d472c4eaa2b5724b4ad40a09
Divesh Dhek <workwithdivesh@gmail.com>
2025-05-11 12:38:38 +0530

    chore: configure Redux store and render app with router and toast support

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import rootReducer from './reducer';
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
