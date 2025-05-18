2220f573d8c5c8049dae8078a799c84709e16d3f
Divesh Dhek <workwithdivesh@gmail.com>
2025-05-18 19:07:45 +0530

    feat(app): configure Routes with public, OpenRoute, PrivateRoute, and catch-all

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+
 import Navbar from './components/common/Navbar';
 import OpenRoute from './components/core/Auth/OpenRoute';
+import PrivateRoute from './components/core/Auth/PrivateRoute';
+
+import About from './pages/About';
 import Error from './pages/Error';
 import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
@@ -9,14 +13,21 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import UpdatePassword from './pages/UpdatePassword';
 import VerifyEmail from './pages/VerifyEmail';
-import About from './pages/About';
+
+import MyProfile from './components/core/Dashboard/MyProfile';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="flex-1 w-screen bg-[var(--color-richblack-900)] flex flex-col font-[var(--font-inter)]">
       <Navbar />
+
       <Routes>
+        {/** 1) Public pages (no guard) */}
         <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+
+        {/** 2) Unauthenticated-only pages */}
         <Route
           path="login"
           element={
@@ -57,23 +68,27 @@ function App() {
             </OpenRoute>
           }
         />
-        <Route
-          path="about"
-          element={
-            <OpenRoute>
-              <About />
-            </OpenRoute>
-          }
-        />
 
+        {/** 3) Protected dashboard page */}
         <Route
-          path="*"
           element={
-            <OpenRoute>
-              <Error />
-            </OpenRoute>
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
           }
-        />
+        >
+          <Route
+            path="dashboard/my-profile"
+            element={
+              <PrivateRoute>
+                <MyProfile />
+              </PrivateRoute>
+            }
+          />
+        </Route>
+
+        {/** 4) Catch-all error page */}
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
   );
