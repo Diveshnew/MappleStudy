29c400feac3d8f0bf0682ad4964c292a1e0bd96c
Divesh Dhek <workwithdivesh@gmail.com>
2025-05-27 19:33:09 +0530

    chore(app): update App.jsx to include instructor routes for add/edit/my-courses

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,9 @@ import Settings from './components/core/Dashboard/Settings';
 import Contact from './pages/Contact';
 import Dashboard from './pages/Dashboard';
 import { ACCOUNT_TYPE } from './utils/constants';
+import MyCourses from './components/core/Dashboard/MyCourses';
+import AddCourse from './components/core/Dashboard/AddCourse';
+import EditCourse from './components/core/Dashboard/EditCourse';
 
 function App() {
   const { user } = useSelector((state) => state.profile);
@@ -93,6 +96,17 @@ function App() {
               <Route
                 path="dashboard/enrolled-courses"
                 element={<EnrolledCourses />}
+              />
+            </>
+          )}
+          {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
+            <>
+              {/* <Route path="dashboard/instructor" element={<Instructor />} /> */}
+              <Route path="dashboard/add-course" element={<AddCourse />} />
+              <Route path="dashboard/my-courses" element={<MyCourses />} />
+              <Route
+                path="dashboard/edit-course/:courseId"
+                element={<EditCourse />}
               />
             </>
           )}
